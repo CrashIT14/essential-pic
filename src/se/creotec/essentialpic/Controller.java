58809c0c32e7090c9746c9a4468c76c70f91d3cb
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-03-20 13:55:13 +0100

    Implements image zooming

--- src/se/creotec/essentialpic/Controller.java
+++ src/se/creotec/essentialpic/Controller.java
@@ -3,7 +3,9 @@ package se.creotec.essentialpic;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,10 +17,14 @@ public class Controller implements Initializable {
     @FXML
     private Pane imageContainer;
 
+    private final Rectangle imageClippingRect = new Rectangle(0, 0);
+
     @FXML
     private void onAboutMenuClicked() {
         Util.getLogger().finest("About dialog");
         Util.showAboutDialog();
+        System.out.println(imageContainer.getWidth());
+        System.out.println(imageMain.getFitWidth());
     }
 
     @FXML
@@ -26,9 +32,32 @@ public class Controller implements Initializable {
         System.out.println("Close");
     }
 
+    @FXML
+    private void onImageContainerScroll(ScrollEvent ev) {
+        if (ev.getDeltaY() < 0) {
+//            imageMain.setScaleX(imageMain.getScaleX() + 0.1);
+//            imageMain.setScaleY(imageMain.getScaleY() + 0.1);
+            imageMain.setFitWidth(imageMain.getFitWidth() * 1.1);
+            imageMain.setFitHeight(imageMain.getFitHeight() * 1.1);
+        } else {
+//            imageMain.setScaleX(imageMain.getScaleX() - 0.1);
+//            imageMain.setScaleY(imageMain.getScaleY() - 0.1);
+            imageMain.setFitWidth(imageMain.getFitWidth() / 1.1);
+            imageMain.setFitHeight(imageMain.getFitHeight() / 1.1);
+        }
+
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        imageMain.fitWidthProperty().bind(imageContainer.widthProperty());
-        imageMain.fitHeightProperty().bind(imageContainer.heightProperty());
+//        imageMain.fitWidthProperty().bind(imageContainer.widthProperty());
+//        imageMain.fitHeightProperty().bind(imageContainer.heightProperty());
+
+        imageClippingRect.widthProperty().bind(imageContainer.widthProperty());
+        imageClippingRect.heightProperty().bind(imageContainer.heightProperty());
+
+        imageMain.setFitWidth(imageMain.getImage().getWidth());
+        imageMain.setFitHeight(imageMain.getImage().getHeight());
+        imageMain.setClip(imageClippingRect);
     }
 }
